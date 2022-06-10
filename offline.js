546df00ca2c6a2c002518510600aeca6931e0554
Симулятор Гонок 5000 <107195354+simulatorgonok@users.noreply.github.com>
2022-06-10 10:11:57 +0300

    1.0
    A game that is impossible to play without stress!

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1654844673,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/background-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
